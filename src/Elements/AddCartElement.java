9242106f2edd896ecb4cb65e521644740a11a3fa
savita pandey <savipandey82@gmail.com>
2019-09-04 12:25:24 +0545

    done all

--- src/Elements/AddCartElement.java
+++ src/Elements/AddCartElement.java
@@ -25,7 +25,7 @@ public class AddCartElement {
         product.click();
     }
 
-    @FindBy(xpath = ".//button[@class=\"exclusive\"]")
+    @FindBy(xpath = "//button[@class=\"exclusive\"]")
     WebElement addtoCartButton;
 
     public void clickAddToCartButton(){
@@ -38,6 +38,62 @@ public class AddCartElement {
 
     public void clickContinueShpping(){
         continueShopping.click();
+    }
+
+      //******** Second Category ******
+    @FindBy(xpath = "//a[@class=\"sf-with-ul\"][@title=\"Women\"]")
+    WebElement add2ndcart;
+
+    public void click2ndCategory(){
+        add2ndcart.click();
+    }
+    @FindBy(xpath = "//a[@data-id-product=\"4\"][@title=\"Add to cart\"]")
+    WebElement add2ndproduct;
+
+    public void click2ndproduct(){
+        add2ndproduct.click();
+    }
+    @FindBy(xpath = "//a[@class=\"btn btn-default button button-medium\"]")
+    WebElement Checkout;
+
+    public void clickCheckout(){
+        Checkout.click();
+    }
+    @FindBy(xpath = "//a[@class=\"button btn btn-default standard-checkout button-medium\"]")
+    WebElement proceedCheckout;
+
+    public void clickProceed(){
+        proceedCheckout.click();
+    }
+    @FindBy(xpath = "//a[@class=\"button-exclusive btn btn-default\"]")
+    WebElement proceedCheckout2;
+
+    public void clickProceed2(){
+        proceedCheckout2.click();
+    }
+    @FindBy(xpath = "//p[@class=\"checkbox\"]")
+    WebElement termsCondition;
+
+    public void clickTermsCondition(){
+        termsCondition.click();
+    }
+    @FindBy(xpath = "//button[@name=\"processCarrier\"]")
+    WebElement proceedCheckout3;
+
+    public void clickProceed3(){
+        proceedCheckout3.click();
+    }
+    @FindBy(xpath = "//a[@class=\"cheque\"]")
+    WebElement payCheck;
+
+    public void clickPayCheck(){
+        payCheck.click();
+    }
+    @FindBy(xpath = "//button[@class=\"button btn btn-default button-medium\"]")
+    WebElement submitCart;
+
+    public void clickSubmit(){
+        submitCart.click();
     }
 }
 
--- src/Test/LoginTest.java
+++ src/Test/LoginTest.java
@@ -2,6 +2,7 @@ package Test;
 
 import Elements.AddCartElement;
 import Elements.LoginPageElement;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -20,13 +21,26 @@ public class LoginTest extends SetUp {
         AddCartElement addcartobj = new AddCartElement(driver);
         addcartobj.setBtnwoman();
         addcartobj.clickProduct();
-        WebDriverWait wait = new WebDriverWait(driver,10);
-        //Thread.sleep(7000);
-        addcartobj.clickAddToCartButton();
+        Thread.sleep(7000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('.exclusive').click();");
+        //addcartobj.clickAddToCartButton();
         Thread.sleep(3000);
         //click continue shopping
         addcartobj.clickContinueShpping();
+        addcartobj.clickCheckout();
 
        // driver.quit();
+    }
+
+    public void add2nd() {
+        AddCartElement add2ndcartobj = new AddCartElement(driver);
+        add2ndcartobj.click2ndCategory();
+        add2ndcartobj.click2ndproduct();
+        add2ndcartobj.clickProceed();
+        add2ndcartobj.clickProceed2();
+        add2ndcartobj.clickProceed3();
+        add2ndcartobj.clickPayCheck();
+        add2ndcartobj.clickSubmit();
     }
 }
